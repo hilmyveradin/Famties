766fa9cedbf0e228840022cc04eea5a2c4187a67
hilmyveradin <hilmyveradin@gmail.com>
2022-06-12 20:35:56 +0700

    create Home and HomeDetail view controller

---
+++ Famties/Controller/HomeDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeDetailViewController.swift
+//  Famties
+//
+//  Created by Hilmy Veradin on 12/06/22.
+//
+
+import UIKit
+
+class HomeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ Famties/Controller/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  Famties
+//
+//  Created by Hilmy Veradin on 12/06/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
